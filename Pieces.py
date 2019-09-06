896f6aa4424913aaddff63f61d09e2194b74ca48
Jp Unknown <jp.unknown47@gmail.com>
2019-09-06 16:07:13 -0400

    Updated pieces per tip from reddit; Require assistance with displaying pieces

---
+++ Pieces.py
@@ -0,0 +1,21 @@
+import os
+
+Pieces = {
+    'Black': {
+        '♗': os.path.join('Pieces', 'black_b.png'),  # Black Bishop
+        '♔': os.path.join('Pieces', 'black_k.png'),  # Black King
+        '♘': os.path.join('Pieces', 'black_n.png'),  # Black Knight
+        '♙': os.path.join('Pieces', 'black_p.png'),  # Black Pawn
+        '♕': os.path.join('Pieces', 'black_q.png'),  # Black Queen
+        '♖': os.path.join('Pieces', 'black_r.png'),  # Black Rook
+        },
+    
+    'White': {
+        '♝': os.path.join('Pieces', 'white_b.png'),  # White Bishop
+        '♚': os.path.join('Pieces', 'white_k.png'),  # White King
+        '♞': os.path.join('Pieces', 'white_n.png'),  # White Knight
+        '♟': os.path.join('Pieces', 'white_p.png'),  # White Pawn
+        '♛': os.path.join('Pieces', 'white_q.png'),  # White Queen
+        '♜': os.path.join('Pieces', 'white_r.png'),  # White Rook
+        }
+    }
--- chess_gui.py
+++ chess_gui.py
@@ -1,10 +1,7 @@
-import logging
-import os
-
 import pygame
 
+import Pieces
 from src.board import *
-from src.chess_pieces import WSYMBOLS, BSYMBOLS
 
 logging.getLogger().setLevel(logging.DEBUG)
 
@@ -18,15 +15,14 @@ def main():
     It takes a user input, checks that input for validity, and calls move on it if it is valid.
     It terminates once the game is won or a player quits the game.
     """
-    
-    window_size = 256, 256
+
+    window_size = 512, 512
     length_of_unit_square = int(window_size[0] / 8)
     framerate = 60
     
     pygame.init()
     pygame.display.set_caption('Chess')
-    Clock = pygame.time.Clock()
-    
+    clock = pygame.time.Clock()
     
     screen = pygame.display.set_mode(window_size)
 
@@ -37,7 +33,7 @@ def main():
         'Grey': [100, 100, 100],
         'Black': [0, 0, 0]
         }
-    screen.fill(colors['Grey'])
+    screen.fill(colors['Black'])
     board = Board()
 
     def checkerboard():
@@ -46,70 +42,37 @@ def main():
             for square in row:
                 if square.row % 2 == 0:
                     if square.column % 2 == 0:
-                        pygame.draw.rect(screen, colors['White'], pygame.Rect(square.row * length_of_unit_square,
-                                                                              square.column * length_of_unit_square,
-                                                                              length_of_unit_square,
-                                                                              length_of_unit_square))
+                        pygame.draw.rect(screen, colors['White'], pygame.Rect(square.row * length_of_unit_square + .5,
+                                                                              square.column * length_of_unit_square + .5,
+                                                                              length_of_unit_square - 1,
+                                                                              length_of_unit_square - 1))
                     else:
-                        pygame.draw.rect(screen, colors['Black'], pygame.Rect(square.row * length_of_unit_square,
-                                                                              square.column * length_of_unit_square,
-                                                                              length_of_unit_square,
-                                                                              length_of_unit_square))
+                        pygame.draw.rect(screen, colors['Grey'], pygame.Rect(square.row * length_of_unit_square + .5,
+                                                                             square.column * length_of_unit_square + .5,
+                                                                             length_of_unit_square - 1,
+                                                                             length_of_unit_square - 1))
                 else:
                     if square.column % 2 == 1:
-                        pygame.draw.rect(screen, colors['White'], pygame.Rect(square.row * length_of_unit_square,
-                                                                              square.column * length_of_unit_square,
-                                                                              length_of_unit_square,
-                                                                              length_of_unit_square))
+                        pygame.draw.rect(screen, colors['White'], pygame.Rect(square.row * length_of_unit_square + .5,
+                                                                              square.column * length_of_unit_square + .5,
+                                                                              length_of_unit_square - 1,
+                                                                              length_of_unit_square - 1))
                     else:
-                        pygame.draw.rect(screen, colors['Black'], pygame.Rect(square.row * length_of_unit_square,
-                                                                              square.column * length_of_unit_square,
-                                                                              length_of_unit_square,
-                                                                              length_of_unit_square))
+                        pygame.draw.rect(screen, colors['Grey'], pygame.Rect(square.row * length_of_unit_square + .5,
+                                                                             square.column * length_of_unit_square + .5,
+                                                                             length_of_unit_square - 1,
+                                                                             length_of_unit_square - 1))
     
     def place_pieces():
         logging.info('Setting up GUI-Pieces')
         for row in board.game_board:
             for square in row:
-                if square.piece != '☐':
-                    # Black Pieces
-                    if square.piece == BSYMBOLS[1]:  # Black Pawn
-                        image = pygame.image.load(os.path.join('Pieces', 'black_p.png')).convert()
-                        rect = screen.blit(image,
-                                           [square.row * length_of_unit_square, square.column * length_of_unit_square])
-                        pygame.display.update(rect)
-                    if square.piece == BSYMBOLS[2]:  # Black Rook
-                        pass
-                    if square.piece == BSYMBOLS[3]:  # Black Knight
-                        pass
-                    if square.piece == BSYMBOLS[4]:  # Black Bishop
-                        pass
-                    if square.piece == BSYMBOLS[5]:  # Black Queen
-                        pass
-                    if square.piece == BSYMBOLS[6]:  # Black King
-                        pass
-
-                    # White Pieces
-                    if square.piece == WSYMBOLS[1]:  # White Pawn
-                        image = pygame.image.load(os.path.join('Pieces', 'white_p.png')).convert()
-                        rect = screen.blit(image,
-                                           [square.row * length_of_unit_square, square.column * length_of_unit_square])
-                        pygame.display.update(rect)
-                    if square.piece == WSYMBOLS[2]:  # White Rook
-                        pass
-                    if square.piece == WSYMBOLS[3]:  # White Knight
-                        pass
-                    if square.piece == WSYMBOLS[4]:  # White Bishop
-                        pass
-                    if square.piece == WSYMBOLS[5]:  # White Queen
-                        pass
-                    if square.piece == WSYMBOLS[6]:  # White King
-                        pass
-                else:
-                    pass
-
+                if square.piece == '☐':
+                    image = pygame.image.load(Pieces.Pieces[square.color][square.piece])
+                    screen.blit(image, [square.row * length_of_unit_square, square.column * length_of_unit_square])
+    
     while True:  # Main loop
-        Clock.tick(framerate)
+        clock.tick(framerate)
         print(board)
     
         checkerboard()
