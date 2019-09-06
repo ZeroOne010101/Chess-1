868e636b5a9b6b6b30f60ad9536dd54372c4328e
Jp Unknown <jp.unknown47@gmail.com>
2019-09-06 17:59:04 -0400

    Reformatted code now ready to more edits

--- chess_gui.py
+++ chess_gui.py
@@ -1,11 +1,8 @@
-import logging
 import os
 
 import pygame
 
-from Pieces import *
 from src.board import *
-from src.chess_pieces import WSYMBOLS, BSYMBOLS
 
 logging.getLogger().setLevel(logging.DEBUG)
 
@@ -19,19 +16,40 @@ def main():
     It takes a user input, checks that input for validity, and calls move on it if it is valid.
     It terminates once the game is won or a player quits the game.
     """
-
+    image_directory = os.path.join(os.getcwd(), 'Pieces')
+    print(image_directory)
+    
     window_size = 512, 512
     length_of_unit_square = int(window_size[0] / 8)
-    framerate = 60
+    framerate = 100
     
     pygame.init()
     pygame.display.set_caption('Chess')
-    Clock = pygame.time.Clock()
-    
+    clock = pygame.time.Clock()
     
     screen = pygame.display.set_mode(window_size)
 
     # font = pygame.font.SysFont('Arial', 20)
+
+    pieces = {
+        'Black': {
+            '♗': pygame.image.load(os.path.join(image_directory, 'black_b.png')),  # Black Bishop
+            '♔': pygame.image.load(os.path.join(image_directory, 'black_k.png')).convert(),  # Black King
+            '♘': pygame.image.load(os.path.join(image_directory, 'black_n.png')).convert(),  # Black Knight
+            '♙': pygame.image.load(os.path.join(image_directory, 'black_p.png')).convert(),  # Black Pawn
+            '♕': pygame.image.load(os.path.join(image_directory, 'black_q.png')).convert(),  # Black Queen
+            '♖': pygame.image.load(os.path.join(image_directory, 'black_r.png')).convert(),  # Black Rook
+            },
+    
+        'White': {
+            '♝': pygame.image.load(os.path.join(image_directory, 'white_b.png')).convert(),  # White Bishop
+            '♚': pygame.image.load(os.path.join(image_directory, 'white_k.png')).convert(),  # White King
+            '♞': pygame.image.load(os.path.join(image_directory, 'white_n.png')).convert(),  # White Knight
+            '♟': pygame.image.load(os.path.join(image_directory, 'white_p.png')).convert(),  # White Pawn
+            '♛': pygame.image.load(os.path.join(image_directory, 'white_q.png')).convert(),  # White Queen
+            '♜': pygame.image.load(os.path.join(image_directory, 'white_r.png')).convert(),  # White Rook
+            }
+        }
     
     colors = {
         'White': [255, 255, 255],
@@ -73,7 +91,7 @@ def main():
         for row in board.game_board:
             for square in row:
                 if square.piece == '☐':
-                    image = pygame.image.load(Pieces[square.color][square.piece])
+                    image = pieces[square.color][square.piece]
                     screen.blit(image, [square.row * length_of_unit_square, square.column * length_of_unit_square])
     
     while True:  # Main loop
